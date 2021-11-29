79e3c462a2d3242384741b9a0822f247e9d5b8ab
Robbel04 <77888602+Robbel04@users.noreply.github.com>
2021-11-29 09:31:14 +0100

    Add files via upload

---
+++ saPHe dashboard/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿
+namespace saPHe_dashboard
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rounded_button1 = new saPHe_dashboard.Rounded_button();
+            this.rounded_button2 = new saPHe_dashboard.Rounded_button();
+            this.SuspendLayout();
+            // 
+            // rounded_button1
+            // 
+            this.rounded_button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(47)))), ((int)(((byte)(83)))));
+            this.rounded_button1.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(47)))), ((int)(((byte)(83)))));
+            this.rounded_button1.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(36)))), ((int)(((byte)(65)))));
+            this.rounded_button1.BorderRadius = 20;
+            this.rounded_button1.BorderSize = 0;
+            this.rounded_button1.Enabled = false;
+            this.rounded_button1.FlatAppearance.BorderSize = 0;
+            this.rounded_button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.rounded_button1.ForeColor = System.Drawing.Color.White;
+            this.rounded_button1.Location = new System.Drawing.Point(12, 12);
+            this.rounded_button1.Name = "rounded_button1";
+            this.rounded_button1.Size = new System.Drawing.Size(276, 1056);
+            this.rounded_button1.TabIndex = 0;
+            this.rounded_button1.TextColor = System.Drawing.Color.White;
+            this.rounded_button1.UseVisualStyleBackColor = false;
+            // 
+            // rounded_button2
+            // 
+            this.rounded_button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(47)))), ((int)(((byte)(83)))));
+            this.rounded_button2.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(47)))), ((int)(((byte)(83)))));
+            this.rounded_button2.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(36)))), ((int)(((byte)(65)))));
+            this.rounded_button2.BorderRadius = 20;
+            this.rounded_button2.BorderSize = 0;
+            this.rounded_button2.Enabled = false;
+            this.rounded_button2.FlatAppearance.BorderSize = 0;
+            this.rounded_button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.rounded_button2.ForeColor = System.Drawing.Color.White;
+            this.rounded_button2.Location = new System.Drawing.Point(306, 12);
+            this.rounded_button2.Name = "rounded_button2";
+            this.rounded_button2.Size = new System.Drawing.Size(1596, 1056);
+            this.rounded_button2.TabIndex = 1;
+            this.rounded_button2.TextColor = System.Drawing.Color.White;
+            this.rounded_button2.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(36)))), ((int)(((byte)(65)))));
+            this.ClientSize = new System.Drawing.Size(1920, 1080);
+            this.Controls.Add(this.rounded_button2);
+            this.Controls.Add(this.rounded_button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Rounded_button rounded_button1;
+        private Rounded_button rounded_button2;
+    }
+}
+
